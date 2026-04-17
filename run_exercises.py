909baeaa100447fa3ddbc5e7cdaa2ae0448d5626
dastuschierblau <32822781+dastuschierblau@users.noreply.github.com>
2026-04-17 15:45:47 +0000

    Correcting import errors.

--- run_exercises.py
+++ run_exercises.py
@@ -1,3 +1,9 @@
+import portfolio.data
+import portfolio.report
+import sys
+
 if __name__ == "__main__":
+    print(sys.path)
+
     my_portfolio = portfolio.data.create_portfolio("Retirement")
     portfolio.report.print_report(my_portfolio)
